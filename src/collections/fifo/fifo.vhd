236f2f397f93f2be91a1bdf0caff69fdbca56396
fournier emilien <emilien.fournier@ensta-bretagne.org>
2019-04-15 23:19:16 +0200

    Generic fifo entity, with architecture d : arch d behavior => fifo when is_pingpong=0 => ping_pong_fifo when is_pingpong = 1

--- src/collections/fifo/fifo.vhd
+++ src/collections/fifo/fifo.vhd
@@ -4,14 +4,15 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity fifo is
 	generic
 	(
-		ADDRESS_WIDTH	: integer	:= 8;								-- address width in bits, maximum CAPACITY is 2^(ADDRESS_WIDTH)-1
-		DATA_WIDTH 		: integer	:= 8 							-- data width in bits, the size of a configuration
+		ADDRESS_WIDTH	: integer	:= 6;								-- address width in bits, maximum CAPACITY is 2^(ADDRESS_WIDTH)-1
+		DATA_WIDTH 		: integer	:= 32 							-- data width in bits, the size of a configuration
 	);
 	port 
 	( 
 		clk 			: in  std_logic;								-- clock
 		reset 			: in  std_logic;								 -- when reset is asserted the stream is emptied: size = 0, is_empty = 1, is_full = 0
 		reset_n			: in  std_logic;
+		is_pingpong 		: in std_logic; 
 		pop_enable 	    : in  std_logic; 								    -- read enable 
 		push_enable	    : in  std_logic; 								    -- write enable 
 		data_in 		: in  std_logic_vector(DATA_WIDTH- 1 downto 0);   -- the data that is added when write_enable
